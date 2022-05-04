c8d7882d32920d3fd738f27f15d18f0f57b05e29
Ed Chalstrey <edwardchalstrey@gmail.com>
2022-05-04 16:45:44 +0100

    add torch import

--- scivision_huggingface/model.py
+++ scivision_huggingface/model.py
@@ -1,5 +1,6 @@
 import numpy as np
 from PIL import Image
+import torch
 from transformers import (AutoFeatureExtractor,
                           SwinForImageClassification,
                           ViTFeatureExtractor,
