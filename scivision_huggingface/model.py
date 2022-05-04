37c00cd36ec0c63f6416eeab22d620beee4daf0d
Ed Chalstrey <edwardchalstrey@gmail.com>
2022-05-04 16:18:26 +0100

    refactor classes to use common vit model func

--- scivision_huggingface/model.py
+++ scivision_huggingface/model.py
@@ -16,6 +16,12 @@ def tidy_predict(self, image: np.ndarray) -> str:
     # model predicts one of the 1000 ImageNet classes
     predicted_class_idx = logits.argmax(-1).item()
     return "Predicted class: " + self.pretrained_model.config.id2label[predicted_class_idx]
+    
+    
+def build_vit_model(model_name: str):
+    model = ViTForImageClassification.from_pretrained(model_name)
+    features = ViTFeatureExtractor.from_pretrained(model_name)
+    return model, features
         
 
 class microsoft_swin_tiny_patch4_window7_224:
@@ -31,8 +37,7 @@ class microsoft_swin_tiny_patch4_window7_224:
 class google_vit_base_patch16_224:
     def __init__(self):
         self.model_name = 'google/vit-base-patch16-224'
-        self.pretrained_model = ViTForImageClassification.from_pretrained(self.model_name)
-        self.feature_extractor = ViTFeatureExtractor.from_pretrained(self.model_name)
+        self.pretrained_model, self.feature_extractor = build_vit_model(self.model_name)
 
     def predict(self, image: np.ndarray) -> np.ndarray:
         return tidy_predict(self, image)
@@ -40,10 +45,10 @@ class google_vit_base_patch16_224:
         
 class imjeffhi_pokemon_classifier:
     def __init__(self):
-        device = "cuda" if torch.cuda.is_available() else "cpu"
         self.model_name = 'imjeffhi/pokemon_classifier'
-        self.pretrained_model = ViTForImageClassification.from_pretrained(self.model_name).to(device)
-        self.feature_extractor = ViTFeatureExtractor.from_pretrained(self.model_name)
+        model, self.feature_extractor = build_vit_model(self.model_name)
+        device = "cuda" if torch.cuda.is_available() else "cpu"
+        self.pretrained_model = model.to(device)
 
     def predict(self, image: np.ndarray) -> np.ndarray:
         return tidy_predict(self, image)
